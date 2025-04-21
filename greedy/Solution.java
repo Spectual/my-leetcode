e342e4563d88b964c449896a0cdfa9a1bfc655d0
Spectual <spectual@icloud.com>
2025-04-21 09:19:30 -0400

    Start tree section

--- greedy/Solution.java
+++ greedy/Solution.java
@@ -1,4 +1,4 @@
-class Solution:
+class Solution {
     // 455
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
@@ -38,3 +38,85 @@ class Solution:
         }
         return result;
     }
+
+
+    // 55
+    public boolean canJump(int[] nums) {
+        int i = 0;
+        int cover = 0;
+        while (i <= cover) {
+            cover = Math.max(cover, i + nums[i]);
+            if (cover >= nums.length - 1) return true;
+            i++;
+        }
+        return false;
+    }
+
+
+    // 45
+    public int jump(int[] nums) {
+        int cur = 0;
+        int next = 0;
+        int result = 0;
+
+        if (nums.length == 0) return 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            next = Math.max(next, nums[i] + i);
+            if (i == cur) {
+                if (cur != nums.length - 1) {
+                    cur = next;
+                    result++;
+                    if (cur >= nums.length) break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // 1005
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0 && k > 0) {
+                nums[i] *= -1;
+                k--;
+            }
+        }
+
+        if (k % 2 == 1) {
+            Arrays.sort(nums);
+            nums[0] *= -1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+
+    //  134
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
+        int curSum = 0;
+        int start = 0;
+        int totalSum = 0;
+
+        for (int i = 0; i < len; i++) {
+           curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+
+            if (curSum < 0) {
+                start = (i + 1) % len;
+                curSum = 0;
+            }
+        }
+
+        if (totalSum < 0) return -1;
+        return start;
+    }
+}
---
+++ tree/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    // 94 DFS
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res= new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    public List<Integer> inorder(TreeNode root, List<integer> list) {
+        if (root == null) return null;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+        return list;
+    }
+
+
+    // 102 BFS
+    public List<List<Integer>> resList = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        order(root);
+        return resList;    
+    }
+
+    public void order(TreeNode root) {
+        if (root == null) return;
+        
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+        que.offer(root);
+
+        while(!que.isEmpty()) {
+            List<Integer> itemList = new ArrayList<Integer>();
+            int len = que.size();
+
+            while(len > 0) {
+                TreeNode item = que.poll();
+                if (item.left != null) que.offer(item.left);
+                if (item.right != null) que.offer(item.right);
+                itemList.add(item.val);
+                len--;
+            }
+
+            resList.add(itemList);
+        }
+    }
+}
