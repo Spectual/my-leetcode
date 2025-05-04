685bc57d5f67c4a80461550cb860cd44eb21448c
Spectual <spectual@icloud.com>
2025-05-03 23:27:14 -0400

    Tree: 101

--- tree/Solution.java
+++ tree/Solution.java
@@ -103,4 +103,22 @@ class Solution {
 
         return root;
     }
+
+
+    // 101 Symmetric Tree
+    public boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right != null) return false;
+        else if (left != null && right == null) return false;
+        else if (left == null && right == null) return true;
+        else if (left.val != right.val) return false;
+
+        boolean outside = compare(left.left, right.right);
+        boolean inside = compare(left.right, right.left);
+        boolean result = outside && inside;
+        return result;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        boolean result = compare(root.left, root.right);
+        return result;
+    }
 }
