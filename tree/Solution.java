454f76582b3f013cc74714444b230206ea9b1af5
Spectual <spectual@icloud.com>
2025-05-16 22:20:38 -0400

    Tree: 236

--- tree/Solution.java
+++ tree/Solution.java
@@ -254,4 +254,16 @@ class Solution {
         }
         return res;
     }
+
+    // 236 Lowest Common Ancestor of a Binary Tree
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == p && right == q || left == q && right == p) return root;
+        if (left != null && right == null) return left;
+        else if (right != null && left == null) return right;
+        return null;
+    }
 }
