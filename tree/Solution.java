cde874943eae4dc78c9c6cc67de98a6ca000b341
Spectual <spectual@icloud.com>
2025-05-17 23:07:19 -0400

    Tree: 701

--- tree/Solution.java
+++ tree/Solution.java
@@ -278,4 +278,17 @@ class Solution {
         else if (right != null) return right;
         return null;
     }
+
+
+    // 701 Insert into a Binary Search Tree
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val < val) root.right = insertIntoBST(root.right, val);
+        if (root.val > val) root.left = insertIntoBST(root.left, val);
+
+        return root;
+    }
 }
