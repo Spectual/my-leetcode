01c3ca76ed81e502552c31daa3a96ba6de66346e
Spectual <spectual@icloud.com>
2025-05-18 20:35:35 -0400

    Tree: 450

--- tree/Solution.java
+++ tree/Solution.java
@@ -291,4 +291,32 @@ class Solution {
 
         return root;
     }
+
+
+    // 450 Delete Node in a BST
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            }
+            else if (root.right == null) {
+                return root.left;
+            }
+            else {
+                TreeNode cur = root.right;
+                while(cur.left != null) {
+                    cur = cur.left;
+                }
+                cur.left = root.left;
+                root = root.right;
+                return root;
+            }
+        }
+
+        if (root.val < key) root.right = deleteNode(root.right, key);
+        if (root.val > key) root.left = deleteNode(root.left, key);
+        return root;
+    }
 }
