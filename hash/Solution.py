829a6a09291a7385db7d98169cd4955408a8af4b
Spectual <spectual@icloud.com>
2025-05-31 22:03:58 -0400

    Hash: 454

--- hash/Solution.py
+++ hash/Solution.py
@@ -51,3 +51,19 @@ class Solution:
                 return [record[target-n], i]
             record[n] = i
         return []
+
+
+    # 454 4Sum
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        record = defaultdict(int)
+
+        for n1 in nums1:
+            for n2 in nums2:
+                record[n1+n2] += 1
+
+        result = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                if -n3-n4 in record:
+                    result += record[0-n3-n4]
+        return result
