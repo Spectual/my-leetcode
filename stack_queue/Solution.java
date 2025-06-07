87d258868433ab5f5e614268d3700c8242319f77
Spectual <spectual@icloud.com>
2025-06-07 10:27:45 -0400

    Queue: 225

--- stack_queue/Solution.java
+++ stack_queue/Solution.java
@@ -41,4 +41,35 @@ class MyQueue {
     public boolean empty() {      
         return tmp.isEmpty() && stack.isEmpty();
     }
+
+
+    // 225 Implement Stack using Queues
+    Queue<Integer> que1;
+    Queue<Integer> que2;
+    public MyStack() {
+        que1 = new LinkedList<>();
+        que2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        que2.offer(x);
+        while(!que1.isEmpty()) {
+            que2.offer(que1.poll());
+        }
+        Queue<Integer> tmp = que1;
+        que1 = que2;
+        que2 = tmp;
+    }
+    
+    public int pop() {
+        return que1.poll();
+    }
+    
+    public int top() {
+        return que1.peek();
+    }
+    
+    public boolean empty() {
+        return que1.isEmpty();
+    }
 }
