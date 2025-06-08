c0a4897d04ce2286f448d7a4537219dfcc123514
Spectual <spectual@icloud.com>
2025-06-08 09:06:43 -0400

    Stack: 20

--- stack_queue/Solution.java
+++ stack_queue/Solution.java
@@ -72,4 +72,31 @@ class MyQueue {
     public boolean empty() {
         return que1.isEmpty();
     }
+
+
+    // 20 Valid Parentheses
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            }
+            else if (c == '{') {
+                stack.push('}');
+            }
+            else if (c == '[') {
+                stack.push(']');
+            }
+            else if (stack.isEmpty()) {
+                return false;
+            }
+            else if (c == stack.peek()) {
+                stack.pop();
+            }
+            else return false;
+        }
+        return stack.isEmpty();
+    }
 }
