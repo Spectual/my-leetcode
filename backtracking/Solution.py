44d24a05e44737dfdc3289072aad899c2a900268
Spectual <spectual@icloud.com>
2025-06-10 11:50:45 -0400

    Backtracking: 216

--- backtracking/Solution.py
+++ backtracking/Solution.py
@@ -15,6 +15,24 @@ class Solution:
             self.path.pop()
 
 
+    #216 Combination Sum III
     def combine(self, n: int, k: int) -> List[List[int]]:
         self.backtracking(n, 1, k)
         return self.result
+
+        def backtracking(self, start_index: int, k: int, n: int, path: List[int], result: List[List[int]]):
+        if len(path) == k:
+            if sum(path) == n:
+                result.append(path[:])
+            return
+        
+        for i in range(start_index, 10):
+            path.append(i)
+            self.backtracking(i+1, k, n, path, result)
+            path.pop()
+
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        result = []
+        path = []
+        self.backtracking(1, k, n, path, result)
+        return result
