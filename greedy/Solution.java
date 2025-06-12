f83d44f2f1c7279b5f808e1a61382ea8d24a6325
Spectual <spectual@icloud.com>
2025-06-12 10:31:58 -0400

    Greedy: 125

--- greedy/Solution.java
+++ greedy/Solution.java
@@ -119,4 +119,27 @@ class Solution {
         if (totalSum < 0) return -1;
         return start;
     }
+
+
+    // 125 Candy
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        int[] candyVec = new int[len];
+        candyVec[0] = 1;
+        for (int i = 1; i < len; i++) {
+            candyVec[i] = (ratings[i] > ratings[i - 1]) ? candyVec[i - 1] + 1 : 1;
+        }
+
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candyVec[i] = Math.max(candyVec[i], candyVec[i + 1] + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int num : candyVec) {
+            ans += num;
+        }
+        return ans;
+    }
 }
