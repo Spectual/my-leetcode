1de62450fbd9bcd21a3ce7a3877db6324cc83fc3
Spectual <spectual@icloud.com>
2025-06-13 17:26:50 -0400

    Greedy: 452

--- greedy/Solution.py
+++ greedy/Solution.py
@@ -20,3 +20,28 @@ class Solution:
                 else:
                     return False
         return True
+
+
+    # 406 Queue Reconstruction by Height
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        sorted_people = sorted(people, key=lambda x: (-x[0], x[1]))
+        result = []
+        for p in sorted_people:
+            result.insert(p[1], p)
+        return result
+
+
+    # 452 Minimum Number of Arrows to Burst Balloons
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        sorted_points = sorted(points, key=lambda x: x[0])
+        cnt = 1
+        min_edge = sorted_points[0][1]
+        for p in sorted_points:
+            if p[0] <= min_edge and p[1] >= min_edge:
+                pass
+            elif p[1] < min_edge:
+                min_edge = p[1]
+            else:
+                min_edge = p[1]
+                cnt += 1
+        return cnt
