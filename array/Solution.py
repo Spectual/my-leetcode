34d28000c5bfc2c4b6d08381e62f12a696ebe646
Spectual <spectual@icloud.com>
2025-06-16 22:35:18 -0400

    Array: 189

--- array/Solution.py
+++ array/Solution.py
@@ -125,3 +125,29 @@ class Solution:
                 k += 1
             i += 1
         return k
+
+
+    # 169 Majority Element
+    def majorityElement(self, nums: List[int]) -> int:
+        record = defaultdict(int)
+        res = 0
+
+        for n in nums:
+            record[n] += 1
+            if record[n] > len(nums) // 2:
+                res = n
+        return res
+
+
+    # 189 Rotate Array
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        res = [0] * n
+        for i in range(n):
+            res[(i + k) % n] = nums[i]
+        for i in range(n):
+            nums[i] = res[i]
