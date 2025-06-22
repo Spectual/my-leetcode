2eb26b1ca148b69d3b1f249f5d515f81d9bc1740
Spectual <spectual@icloud.com>
2025-06-22 09:24:31 -0400

    Tree: 100

--- tree/Solution.java
+++ tree/Solution.java
@@ -319,4 +319,20 @@ class Solution {
         if (root.val > key) root.left = deleteNode(root.left, key);
         return root;
     }
+
+
+    // 100 Tree Node
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }        
+        else if (p == null || q == null) {
+            return false;
+        }
+
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        return left && right && (p.val == q.val);
+    }
 }
