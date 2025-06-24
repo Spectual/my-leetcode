2436c0ea3c62ba24ef069097046e575991d7c2b3
Spectual <spectual@icloud.com>
2025-06-24 18:03:45 -0400

    DP: 62

--- dp/Solution.py
+++ dp/Solution.py
@@ -31,3 +31,19 @@ class Solution:
         for i in range(2, len(cost)+1):
             dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
         return dp[len(cost)]
+
+
+    # 62 Unique Paths
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    pass
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j]
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
