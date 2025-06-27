8bae39d3eb4567abfb63c46117967ab70e5aac80
Spectual <spectual@icloud.com>
2025-06-26 21:05:31 -0400

    DP: 96

--- dp/Solution.py
+++ dp/Solution.py
@@ -87,3 +87,14 @@ class Solution:
             for j in range(1, i // 2 + 1):
                 dp[i] = max([j * (i - j), j * dp[i - j], dp[i]])
         return dp[n]
+    
+
+    # 96 Unique Binary Search Trees
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for i in range(1, n+1):
+            for j in range(0, i):
+                dp[i] += dp[j] * dp[i-j-1]
+        return dp[n]
