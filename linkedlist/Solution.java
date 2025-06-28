4b68884881865af1f9d5c09392ffb5f3eb613746
Spectual <spectual@icloud.com>
2025-06-28 11:00:59 -0400

    LinkedList: 2

--- linkedlist/Solution.java
+++ linkedlist/Solution.java
@@ -156,4 +156,27 @@ class Solution {
         return dummy.next;        
     }
 
+
+    // 2 Add Two Numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode res = dummy;
+        int total = 0, carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            total = carry;
+            if (l1 != null) {
+                total += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                total += l2.val;
+                l2 = l2.next;
+            }
+            carry = total / 10;
+            int num = total % 10;
+            res.next = new ListNode(num);
+            res = res.next;
+        }
+        return dummy.next;
+    }
 }
