e84d306e5f420e3b5f5cfc5a4c53e190414f62de
Spectual <spectual@icloud.com>
2025-07-05 19:04:43 -0400

    Graph: 200

--- graph/Solution.py
+++ graph/Solution.py
@@ -61,3 +61,31 @@ class Solution:
             for i in range(len(result)):
                 print(" ".join(map(str, result[i])))
 
+
+    #  200 Number of Islands
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = []
+        res = 0
+        row = len(grid)
+        col = len(grid[0])
+        if not grid:
+            return res
+
+        def dfs(grid, x, y, row, col):
+            if x < 0 or x > row - 1 or y < 0 or y > col - 1:
+                return
+            if grid[x][y] == "0":
+                return
+            else:
+                grid[x][y] = "0"
+            dfs(grid, x-1, y, row, col)
+            dfs(grid, x, y-1, row, col)
+            dfs(grid, x+1, y, row, col)
+            dfs(grid, x, y+1, row, col)
+            
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1":
+                    res += 1
+                    dfs(grid, i, j, row, col)
+        return res   
