6e1e3d1451f51042619fe86effeff6112aea7ae5
Spectual <spectual@icloud.com>
2025-07-09 09:49:22 -0400

    LinkedList: 19

--- linkedlist/Solution.py
+++ linkedlist/Solution.py
@@ -53,3 +53,71 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # 92 Reverse Linked List II
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0, head)
+        prev = dummy
+        for _ in range(left-1):
+            prev = prev.next
+
+        cur = prev.next
+        for _ in range(right-left):
+            temp = cur.next
+            cur.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
+    
+
+    # 61 Rotate List
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        dummy = ListNode(0, head)
+        length = 0
+        while dummy.next:
+            dummy = dummy.next
+            length += 1
+        
+        position = k % length
+        if position == 0:
+            return head
+            
+        cur = head
+
+        for _ in range(length - position - 1):
+            cur = cur.next
+        
+        new_head = cur.next
+        dummy.next = head
+        cur.next = None
+        return new_head
+    
+
+    # 19 Remove Nth Node From End of List
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = dummy
+
+        for _ in range(n):
+            right = right.next
+
+        while right.next:
+            right = right.next
+            left = left.next
+            
+        left.next = left.next.next
+        return dummy.next
