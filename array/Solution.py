ee9b0919afa73c62e1b1376b439914929a931919
Spectual <spectual@icloud.com>
2025-07-22 11:18:37 -0400

    Array: 6

--- array/Solution.py
+++ array/Solution.py
@@ -309,3 +309,25 @@ class Solution:
         else:
             ranges.append(str(nums[start])+"->"+str(nums[-1]))
         return ranges
+
+
+    # 6 Zigzag Conversion
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [[] for _ in range(numRows)]
+        idx, d = 0, 1
+
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+
+        for i in range(numRows):
+            rows[i] = "".join(rows[i])
+
+        return "".join(rows)
