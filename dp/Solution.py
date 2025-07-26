00c6ca5db9f534194987bfeb612d4bd21e8d6043
Spectual <spectual@icloud.com>
2025-07-25 23:51:02 -0400

    DP: 72

--- dp/Solution.py
+++ dp/Solution.py
@@ -178,18 +178,19 @@ class Solution:
         return min(dp[n-1])
     
 
-    # 64 Minimum Path Sum
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = grid[0][0]
-
-        for i in range(1, m):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
-        for j in range(1, n):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
-        return dp[m-1][n-1]
+    # 72 Edit Distance
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            dp[i][0] = i
+        for j in range(1, n+1):
+            dp[0][j] = j
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word2[j-1] == word1[i-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:    
+                    dp[i][j] = min(dp[i][j-1]+1, dp[i-1][j]+1, dp[i-1][j-1]+1)
+        return dp[m][n]
