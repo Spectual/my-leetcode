b819cac096a17854ef3a65ed6e2f3e4c4872f225
Spectual <spectual@icloud.com>
2025-07-26 11:21:55 -0400

    DP: 97

--- dp/Solution.py
+++ dp/Solution.py
@@ -194,3 +194,23 @@ class Solution:
                 else:    
                     dp[i][j] = min(dp[i][j-1]+1, dp[i-1][j]+1, dp[i-1][j-1]+1)
         return dp[m][n]
+    
+
+    # 97 Interleaving String
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        m, n, l = len(s1), len(s2), len(s3)
+        if m + n != l:
+            return False
+            
+        dp = [[False] * (n+1) for _ in range(m+1)]
+
+        dp[0][0] = True
+        for i in range(1, m+1):
+            dp[i][0] = dp[i-1][0] and s3[i-1] == s1[i-1]
+        for j in range(1, n+1):
+            dp[0][j] = dp[0][j-1] and s3[j-1] == s2[j-1]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = (dp[i-1][j] and s3[i+j-1]==s1[i-1]) or (dp[i][j-1] and s3[i+j-1]==s2[j-1])
+
+        return dp[m][n]
