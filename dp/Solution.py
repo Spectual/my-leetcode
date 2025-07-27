60d27ff29654061624f852d4bc30987c7fc4fa79
Spectual <spectual@icloud.com>
2025-07-27 12:25:21 -0400

    DP: 221

--- dp/Solution.py
+++ dp/Solution.py
@@ -214,3 +214,23 @@ class Solution:
                 dp[i][j] = (dp[i-1][j] and s3[i+j-1]==s1[i-1]) or (dp[i][j-1] and s3[i+j-1]==s2[j-1])
 
         return dp[m][n]
+    
+
+    # 221 Maximal Square
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        max_len = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(
+                            dp[i-1][j], dp[i][j-1], dp[i-1][j-1]
+                        ) + 1
+                    max_len = max(max_len, dp[i][j])
+        return max_len ** 2
